5a6d33cc9701b2fedbf01d99733b14d31e7ff3c4
DylGit3 <senddylmail27@gmail.com>
2025-04-13 02:57:30 -0700

    Initial Commit

---
+++ Pause/PauseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PauseApp.swift
+//  Pause
+//
+//  Created by Dylan Geraci on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct PauseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
